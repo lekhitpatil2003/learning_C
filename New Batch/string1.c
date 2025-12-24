7f360458aead0474ed4baffa395719db98bdc518
Lekhit Patil <patillekhit@gmail.com>
2025-12-24 13:09:44 +0530

    solve some examples

--- New Batch/string1.c
+++ New Batch/string1.c
@@ -11,11 +11,11 @@ int main(){
 //	fgets(fullname,sizeof(fullname),stdin);
 //	printf("Your Full Name is = %s",fullname);
 	
-//	//strlen
+	//strlen
 //	char name[] = "Lekhit Sanjay Patil";
 //	printf("Length = %d",strlen(name));
 //	
-//	//strcpy
+	//strcpy
 //	char a[20] = "Hello";
 //	char b[20];
 //	strcpy(b,a);
@@ -34,15 +34,33 @@ int main(){
 //	strcat(x,z);
 //	printf("\n%s",x);
 
-	char s1[10] = "hello";
-	char s2[10] = "Hello";
-	if(strcasecmp(s1,s2) == 0){
-		printf("String are Equal");
-	}else{
-		printf("String are not equal");
-	}
+//	char s1[10] = "hello";
+//	char s2[10] = "Hello";
+//	if(strcasecmp(s1,s2) == 0){
+//		printf("String are Equal");
+//	}else{
+//		printf("String are not equal");
+//	}
 	
+	//print in upper case
+//	char str[] = "Hello it's Lekhit";
+//	strupr(str);
+//	printf("%s\n",str);	
+//	
+	//print in lower case
+//	strlwr(str);
+//	printf("%s\n",str);
 	
+	//string print in rev
+//	char str1[] = "programming";
+//	strrev(str1);
+//	printf("%s",str1);
+
+	char str[] = "lekhit";
+    int len;
+    len = strlen(str);
+    printf("First letter: %c\n", str[0]);
+    printf("Last letter : %c\n", str[len - 1]);
 	
 	return 0;
 }
